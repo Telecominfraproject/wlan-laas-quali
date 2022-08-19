81fd898b98a8e32f241c92182df6502377699659
Rohan Dutta <rohandutta@fb.com>
2022-08-19 01:09:08 -0700

    Added JQ command to be installed and modified shell drivers which were zip files
    Signed-off-by: dutta-rohan <94938704+dutta-rohan@users.noreply.github.com>

--- Scripts/terminal_server_setup.py
+++ Scripts/terminal_server_setup.py
@@ -1,13 +1,15 @@
 import sys
 import os
 #install packages
-package_list = ['paramiko','dlipower','pytest','scp']
+package_list = ['paramiko','dlipower','pytest','scp','jq']
 for package_name in package_list:
     try:
-os.system('sudo pip install '+package_name)
-print("------------INSTALLED: "+package_name+"-------------")
+        os.system('sudo pip install '+package_name)
+        print("------------INSTALLED: "+package_name+"-------------")
     except:
-print("Cannot install "+package_name)
+        print("Cannot install "+package_name)
+        if package_name=="jq":
+            print("Try running: sudo yum install jq")
 
 #copy openwrt_ctl.py file
 os.system('cd cicd-git/;git clone https://github.com/Telecominfraproject/wlan-testing/; cp /home/lanforge/cicd-git/wlan-testing/libs/apnos/openwrt_ctl.py .;rm -rf wlan-testing/;cd ../')
